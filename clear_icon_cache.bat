eda93c0c3c951620783672cb3072b96893ba1529
piaoyezju <piaoyezju@gmail.com>
2026-05-19 14:55:25 +0800

    chore: remove clear_icon_cache.bat

--- clear_icon_cache.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-taskkill /f /im explorer.exe >nul 2>&1
-del /f /q "%LocalAppData%\IconCache.db" >nul 2>&1
-del /f /q "%LocalAppData%\Microsoft\Windows\Explorer\iconcache*" >nul 2>&1
-del /f /q "%LocalAppData%\Microsoft\Windows\Explorer\thumbcache*" >nul 2>&1
-start explorer.exe
-echo Done.
-pause
